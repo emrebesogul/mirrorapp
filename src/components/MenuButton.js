f82d2a0301a6d9500859da0680a8f77f1bb7f1f0
Emre Besogul <emre@Emres-MacBook-Pro.local>
2019-03-13 13:21:19 +0100

    Fix native base error on all screens

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -4,13 +4,14 @@ import { Ionicons } from '@expo/vector-icons';
 import {
     View,
     Text,
+    Button
 } from 'react-native';
 
 export default class MenuButton extends React.Component {
 
     render() {
         return (
-            <Ionicons name="md-menu" color="#000000" size={32} onPress={() => this.props.navigation.toggleDrawer()} />
+            <Button title="Menu" onPress={() => this.props.navigation.toggleDrawer()} />
         )
     }
 }
